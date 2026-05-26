45ca958195ab4a606ff781028bf521fdbcc7af22
Waqasalee <mypakskn@gmail.com>
2026-05-26 15:35:31 +0500

    Add check_count

---
+++ check_count.py
@@ -0,0 +1,18 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_URL = os.getenv("DATABASE_URL")
+
+def main():
+    conn = psycopg2.connect(DB_URL)
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM "Channel";')
+    count = cur.fetchone()[0]
+    print(f"Total Channels in DB: {count}")
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
