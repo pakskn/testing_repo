11690532c48e93b23f11ed2cd41cebff37ce2f09
Waqasalee <mypakskn@gmail.com>
2026-05-26 13:38:25 +0500

    Add check_ts_urls.py

---
+++ check_ts_urls.py
@@ -0,0 +1,27 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_URL = os.getenv("DATABASE_URL")
+
+def main():
+    conn = psycopg2.connect(DB_URL)
+    cur = conn.cursor()
+
+    cur.execute('''
+        SELECT "videoId", "thumbnailUrl"
+        FROM "Video"
+        WHERE "channelId" = (SELECT "channelId" FROM "Channel" WHERE "channelName" = 'Travel Smarter' LIMIT 1)
+    ''')
+    videos = cur.fetchall()
+    
+    print("Travel Smarter URLs:")
+    for vid_id, thumb_url in videos:
+        print(f"[{vid_id}] -> {repr(thumb_url)}")
+            
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
