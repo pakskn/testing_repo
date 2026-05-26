8b0fb3277c4872ffb38742c1c5804b879ffa0126
Waqasalee <mypakskn@gmail.com>
2026-05-26 15:45:09 +0500

    Add check_videos

--- check_videos.py
+++ check_videos.py
@@ -8,19 +8,12 @@ DB_URL = os.getenv("DATABASE_URL")
 def main():
     conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
-
-    cur.execute('''
-        SELECT title, "videoId", duration, views
-        FROM "Video"
-        WHERE "channelId" = (SELECT "channelId" FROM "Channel" WHERE "channelName" = 'Political Decrypt' LIMIT 1)
-        ORDER BY views DESC
-    ''')
-    videos = cur.fetchall()
-    
-    print("Videos for Political Decrypt:")
-    for v in videos:
-        print(f"[{v[2]}] {v[1]} | {v[3]} views | {v[0]}")
-            
+    cur.execute('SELECT COUNT(*) FROM "Video";')
+    count = cur.fetchone()[0]
+    print(f"Total Videos in DB: {count}")
+    cur.execute('''SELECT COUNT(*) FROM "Channel" WHERE "channelType" = 'long';''')
+    ch_count = cur.fetchone()[0]
+    print(f"Total Long Channels in DB: {ch_count}")
     cur.close()
     conn.close()
 
